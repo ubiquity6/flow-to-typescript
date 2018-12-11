95db33707edede8f8f98c653ec48c4a7056f187f
Nicolas Coderre <nicolas.coderre@gmail.com>
2018-12-10 23:33:56 -0800

    fix generic_constraint test that was pointing to wrong thing update snapshot.

--- tests/test-generic_constraint.ts
+++ tests/test-generic_constraint.ts
@@ -3,7 +3,7 @@ import { convert,analyseref } from '../src/index';
 
 describe('generic_constraint', () => {
     it('main', () =>
-      expect(convert(null, 'tests/src/constructor_interface.js', {dump_ast: true, 
+      expect(convert(null, 'tests/src/generic_constraint.js', {dump_ast: true, 
         no_emit: true,
         print_types: false
     })).toMatchSnapshot()
